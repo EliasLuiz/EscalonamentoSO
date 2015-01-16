660781a080f1bf80a79f36ac4e3e2511cdf2ee8f
EliasLuiz <eliasluizjr@gmail.com>
2015-01-16 10:55:03 -0200

    Adicionado Escalonador Tempo Compartilhado

---
+++ Escalonamento/Escalonamento/lista.h
@@ -0,0 +1,287 @@
+#include<stdlib.h>
+
+#ifndef LISTA_GENERICA
+#define LISTA_GENERICA
+
+
+/*
+	Estrutura de uma lista generica.
+	- Os conteudos armazenados por esta lista sao ponteiros void
+	portanto pode-se armazenar qualquer tipo de dados, porem
+	o usuario deve fazer o casting para utilizar o valor, ja
+	que os valores de retorno tambem sao ponteiros void.
+	- Tambem eh importante que se aloque uma posicao nova de memoria
+	para o elemento que se quer inserir na lista antes de o inserir.
+		Ex: Caso se insira na lista a variavel x e apos inserir x seja alterado
+		fora da lista, x sera alterado dentro da lista, ja que a lista so
+		armazena ponteiros.
+*/
+
+/*
+	ADICIONAR:
+		-SORT
+		-INSERE POR INDICE
+		-LE POR INDICE
+		-REMOVE POR INDICE
+*/
+
+#pragma region Estruturas
+
+typedef struct Celula *PonteiroCelula;
+
+typedef struct Celula
+//estrutura de dados que armazena cada elemento da lista
+{
+	void *p;
+	PonteiroCelula prox;
+	PonteiroCelula ant;
+	//Celula.p aponta para o conteudo da celula (item) [Ex: int]
+	//Celula.tam armazena o tamanho (em bytes) do conteudo (item) [Ex: 4]
+} Celula;
+
+typedef struct
+//estrutura de dados usado para a lista
+{
+	PonteiroCelula primeiro;
+	PonteiroCelula ultimo;
+	int tam;
+	//Lista.primeiro aponta para a celula cabeça da lista
+	//Lista.ultimo aponta para a celula rabo da lista
+	//todos os elementos da lista ficaram entre essas 2 celulas
+	//Lista.tam guarda o numero de elementos na lista
+} Lista;
+
+#pragma endregion Definicao das estruturas de dados basicas da lista
+
+#pragma region Utilidades
+
+int EstaVaziaLista(Lista l)
+// l aponta para a lista a ser checada
+// retorna true se a lista esta vazia, false se nao
+{
+	return !(l.tam);
+}
+
+void InicializaLista(Lista *l)
+// l aponta para a lista a ser inicializada
+// inicializa uma lista como estando vazia (necessario para evitar erros de segmentacao)
+{
+	l->primeiro = (Celula*)malloc(sizeof(Celula));
+	l->ultimo = (Celula*)malloc(sizeof(Celula));
+	//cria celula cabeca e celula rabo
+
+	l->tam = 0;
+	//marca a lista como vazia
+
+	l->primeiro->prox = l->ultimo;
+	l->primeiro->ant = NULL;
+	l->ultimo->ant = l->primeiro;
+	l->ultimo->prox = NULL;
+	//linka as duas celulas base
+
+	l->primeiro->p = NULL;
+	l->ultimo->p = NULL;
+	//coloca as duas celulas com conteudo vazio
+}
+
+int TamanhoLista(Lista l)
+// l eh a lista a ter o tamanho checado
+{
+	return l.tam;
+}
+
+void* PeekLista(Lista l)
+// l eh a lista a ter o elemento checado
+// retorna um ponteiro para o primeiro elemento da lista, mantendo-o na lista
+// caso não haja elementos retorna NULL
+{
+	if (l.tam > 0)
+		return l.primeiro->prox->p;
+	else
+		return NULL;
+}
+
+#pragma endregion Funcoes variadas relacionadas a lista
+
+#pragma region Insercao
+
+void InsereFimLista(Lista *l, void *p)
+// l aponta para a lista a ter o elemento inserido
+// p aponta para o elemento a ser inserido na lista
+// insere uma celula apontando para p ao final da lista
+{
+	Celula *aux, *q;
+	aux = l->ultimo->ant;
+	q = (Celula*)malloc(sizeof(Celula));
+	q->p = p;
+	//inicializa aux com o ultimo elemento da lista
+	//e q com uma celula contendo p
+
+	aux->prox = q;
+	q->ant = aux;
+	//insere q depois de aux
+
+	l->ultimo->ant = q;
+	q->prox = l->ultimo;
+	//indica que q eh o novo ultimo elemento
+
+	l->tam++;
+}
+
+void InsereInicioLista(Lista *l, void *p)
+// l aponta para a lista a ter o elemento inserido
+// p aponta para o elemento a ser inserido na lista
+// insere uma celula apontando para p no inicio da lista
+{
+	Celula *aux, *q;
+	aux = l->primeiro->prox;
+	q = (Celula*)malloc(sizeof(Celula));
+	q->p = p;
+	//inicializa aux com o primeiro elemento da lista
+	//e q com uma celula contendo p
+
+	aux->ant = q;
+	q->prox = aux;
+	//insere q antes de aux
+
+	l->primeiro->prox = q;
+	q->ant = l->primeiro;
+	//indica que q eh o novo primeiro elemento
+
+	l->tam++;
+}
+
+void InsereOrdenadoLista(Lista *l, void *p, int(*f)(void*, void*))
+// l aponta para a lista a ter o elemento inserido
+// p aponta para o elemento a ser inserido na lista
+// f aponta para uma funcao que retorna true caso o primeiro parametro deva ficar antes do segundo
+{
+	Celula *a, *b, *c;
+
+	b = (Celula*)malloc(sizeof(Celula));
+	b->p = p;
+	//cria b, a celula que ira conter p
+
+	a = l->primeiro;
+	c = a->prox;
+	//inicia c com o primeiro elemento
+
+	while (c != l->ultimo && f(c->p, p))
+	{
+		a = c;
+		c = c->prox;
+	}
+	//enquanto f for verdadeira "caminha" com a posicao em que p sera inserido
+	//ira parar quando c for o elemento a ficar depois de p (logo, a ficara antes)
+
+	a->prox = b;
+	b->ant = a;
+	b->prox = c;
+	c->ant = b;
+	//insere b entre a e c
+
+	l->tam++;
+}
+
+#pragma endregion Funcoes que inserem na lista
+
+#pragma region Remocao
+
+void* RetiraFimLista(Lista *l)
+// l aponta para a lista a ter o elemento retirado
+// retorna um ponteiro para o item "guardado" pela celula ao fim da lista
+// retorna NULL caso a lista esteja vazia
+{
+	if (l->tam == 0)
+		return NULL;
+		//caso a lista esteja vazia retorna NULL
+
+	Celula *p, *q;
+	q = l->ultimo->ant;
+	// q recebe o ultimo elemento
+	p = q->ant;
+	// p recebe o penultimo elemento
+
+	p->prox = l->ultimo;
+	l->ultimo->ant = p;
+	//indica que p eh o novo ultimo elemento
+
+	void *aux;
+	aux = q->p;
+	// armazena o conteudo de q em aux
+
+	free(q);
+
+	l->tam--;
+
+	return aux;
+}
+
+void* RetiraInicioLista(Lista *l)
+// l aponta para a lista a ter o elemento retirado
+// retorna um ponteiro para o item "guardado" pela celula no inicio da lista
+// retorna NULL caso a lista esteja vazia
+{
+	if (l->tam == 0)
+		return NULL;
+	//caso a lista esteja vazia retorna NULL
+
+	Celula *p, *q;
+	q = l->primeiro->prox;
+	// q recebe o primeiro elemento
+	p = q->prox;
+	// p recebe o segundo elemento
+
+	p->ant = l->primeiro;
+	l->primeiro->prox = p;
+	//indica que p eh o novo primeiro elemento
+
+	void *aux;
+	int *teste;
+	aux = q->p;
+	// armazena o conteudo de q em aux
+
+	free(q);
+
+	l->tam--;
+
+	teste = (int*)aux;
+
+	return aux;
+}
+
+void* RetiraElementoLista(Lista *l, int(*f)(void*, void*), void* x)
+// l aponta para a lista a ter o elemento retirado
+// f aponta para a funcao que retorna true caso o elemento deva ser retirado
+// f recebe 2 parametros, decidindo se o 1o deve ser retirado baseado em uma comparacao com o 2o
+// x eh o segundo parametro com o qual f decide se o elemento deve ser retirado
+// retorna um ponteiro para o primeiro elemento em que f retorne true
+// retorna NULL caso nenhum elemento 
+{
+	if (l->tam == 0)
+		return NULL;
+	//caso a lista esteja vazia retorna NULL
+	
+	Celula *p;
+	p = l->primeiro->prox;
+	//inicia p com o primeiro elemento da lista
+
+	while (p != l->ultimo && !f(p->p, x))
+	{
+		p = p->prox;
+	}
+	//enquanto f for falso "caminha" com o ponteiro p
+	//ira parar quando p conter o elemento a ser retirado ou chegar ao fim da lista
+
+	void *aux;
+	aux = p->p;
+	//aux recebe o elemento a ser retornado
+
+	free(p);
+
+	return aux;
+}
+
+#pragma endregion Funcoes que removem da lista
+
+#endif
---
+++ Escalonamento/Escalonamento/main.c
@@ -0,0 +1,380 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "lista.h"
+
+
+#pragma region Declaracoes Iniciais
+typedef struct
+//Tipo de dados Processo, que serve como base para a simulacao
+{
+	int id;
+	int tempoCpu;
+	int tempoChegada;
+	int prioridade;
+} Processo;
+
+int ins_tempoCriacao(void *a, void *b);
+int ins_tempoCPU(void *a, void *b);
+int ins_prioridade(void *a, void *b);
+
+void Enfileira(Lista *l, Processo p, int algoritmo);
+#pragma endregion Prototipos de funcoes e declaracao de tipos da dados usados na simulacao
+
+
+int main()
+{
+	//----------------------------------------------------
+	// PARAMETROS DA SIMULACAO
+	//----------------------------------------------------
+	#pragma region Parametros da simulacao
+
+	int algoritmo = 0, nProcessos, tempoTroca = 0, fatiaTempo = 0;
+	Lista filaPronto, filaCriacao;
+	//fila = fila dos processos em pronto
+	//filaaux = fila dos processos a serem criados
+	InicializaLista(&filaPronto);
+	InicializaLista(&filaCriacao);
+
+	printf("1 - FIFO: ");
+	printf("\n2 - SJF: ");
+	printf("\n3 - Circular: ");
+	printf("\n4 - Prioridade: ");
+	printf("\nAlgoritmo: ");
+	while (algoritmo < 1 || algoritmo > 4) //cerca entrada invalida do usuario
+		scanf("%d", &algoritmo);
+	printf("Numero de processos: ");
+	scanf("%d", &nProcessos);
+	printf("Tempo de troca de contexto: ");
+	scanf("%d", &tempoTroca);
+	if (algoritmo == 3)
+	{
+		printf("Fatia de tempo: ");
+		scanf("%d", &fatiaTempo);
+	}
+	//le parametros da simulacao
+
+	#pragma endregion
+
+
+	//----------------------------------------------------
+	// CRIACAO DOS PROCESSOS
+	//----------------------------------------------------
+	#pragma region Criacao dos Processos
+
+	int *vInitProcessos;
+	vInitProcessos = (int*)malloc(nProcessos*sizeof(int));
+	//salva o tempo em que cada processo vai iniciar
+
+	int i;
+	printf("\n\n");
+	for (i = 0; i<nProcessos; i++)
+	{
+		Processo *p;
+		p = (Processo*)malloc(sizeof(Processo));
+		// lista requer uma alocacao de memoria por elemento
+		p->id = i;
+		printf("Processo %d: ", i);
+		printf("\nTempo de Chegada: ");
+		scanf("%d", &p->tempoChegada);
+		printf("Tempo de CPU: ");
+		scanf("%d", &p->tempoCpu);
+		if (algoritmo == 4)
+		{
+			printf("Prioridade: ");
+			scanf("%d", &p->prioridade);
+		}
+		InsereOrdenadoLista(&filaCriacao, p, ins_tempoCriacao);
+
+		vInitProcessos[i] = p->tempoChegada;
+	}
+	//leitura dos dados dos processos
+
+	#pragma endregion
+
+	
+	//----------------------------------------------------
+	// EXECUCAO DA SIMULACAO
+	//----------------------------------------------------
+	#pragma region Execucao da Simulacao
+
+	int tempoTotal = 0;
+	//tempo da cpu
+	//valor final = tempo total de simulacao
+	int tempoRestante = -1;
+	//tempo restante do total alocado para o processo
+	Processo emExec;
+	emExec.id = 0; //processo especial que "executa" no 1o ciclo 
+				   //			(depois eh sobrescrito pelo id=0)
+	emExec.tempoCpu = 0;
+	emExec.prioridade = -1;
+	emExec.tempoChegada = 0;
+	//variavel que armazena qual processo esta em execucao
+	int *vFimProcessos;
+	vFimProcessos = (int*)malloc(nProcessos*sizeof(int));
+	//salva o tempo em que cada processo termina
+	int *vProcessos;
+	vProcessos = NULL;
+	//salva qual processo esta executando
+	//Ex: vProcessos[10] = 3 
+	//		=> no tempo 10 o processo em execucao eh o numero 3
+	//-1 = Ocioso
+	//-2 = Troca de contexto
+
+	while (!EstaVaziaLista(filaCriacao) || !EstaVaziaLista(filaPronto) || tempoRestante >= 0)
+	{
+		//ALGORITMO DE SIMULACAO:
+		//cria processos
+		//olha se vai ter preempcao por prioridade
+		//olha se vai continuar executando
+			//se for executa
+			//se nao, olha se acabou
+				//se terminou descarta
+				//se nao terminou volta pra fila
+
+		Processo *proximo;
+		proximo = (Processo*)PeekLista(filaCriacao);
+		while( proximo != NULL && proximo->tempoChegada <= tempoTotal )
+		//Enquanto o primeiro processo da fila for criado nesse momento
+		{
+			Processo *p;
+			p = RetiraInicioLista(&filaCriacao);
+			Enfileira(&filaPronto, *p, algoritmo);
+			free(p);
+
+			proximo = (Processo*)PeekLista(filaCriacao);
+		} //Retira o processo da fila de criacao e insere na fila de pronto
+
+		proximo = (Processo*)PeekLista(filaPronto);
+		if (algoritmo == 4 && proximo != NULL && proximo->prioridade > emExec.prioridade)
+		//Se deve ocorrer uma preempcao por prioridade
+		{
+			tempoRestante = 0;
+		} //Zera o tempo alocado para o processo atual
+
+
+		if (emExec.tempoCpu == 1)
+		{
+			tempoRestante = 0;
+		} //Olha se processo terminou (escalonamento circular)
+
+		if (tempoRestante > 0)
+		{
+			tempoRestante--;
+			emExec.tempoCpu--;
+		} //Olha se vai continuar executando
+		else
+		{
+			if (emExec.tempoCpu > 1)
+			{
+				emExec.tempoCpu--;
+				Enfileira(&filaPronto, emExec, algoritmo);
+			} //Se o processo nao terminou de executar volta para a fila de pronto
+			else if (emExec.id != -1)
+			{
+				vFimProcessos[emExec.id] = tempoTotal;
+			} //Se o processo terminou armazena o tempo de termino
+
+			proximo = ((Processo*)RetiraInicioLista(&filaPronto));
+			if (proximo == NULL)
+			{
+				emExec.id = -1; //Processo nulo: indica cpu ociosa
+				tempoRestante = -1;
+			}
+			else
+			{
+				emExec = *proximo;
+
+				if (algoritmo == 3)
+				{
+					tempoRestante = fatiaTempo - 1;
+				} // se for circular aloca uma fatia de tempo para o processo
+				else
+				{
+					tempoRestante = emExec.tempoCpu - 1;
+				} // se nao for circular, aloca o tempo necessario
+
+				tempoTotal += tempoTroca;
+				vProcessos = (int*)realloc(vProcessos, tempoTotal * sizeof(int));
+				for (i = tempoTotal - tempoTroca; i < tempoTotal; i++)
+				{
+					vProcessos[i] = -2;
+				}//faz a troca de contexto
+			}
+			//Escalona o proximo processo
+
+		}//Caso precise escalonar um processo novo
+
+		tempoTotal++;
+
+		vProcessos = (int*)realloc(vProcessos, tempoTotal * sizeof(int));
+		vProcessos[tempoTotal - 1] = emExec.id;
+		//Armazena qual processo esta em execucao naquele momento
+	}
+
+	#pragma endregion
+
+
+	//----------------------------------------------------
+	// ANALISE DE RESULTADOS / CRIACAO DA TABELA
+	//----------------------------------------------------
+	#pragma region Analise de Resultados / Criacao da Tabela
+
+	char** matriz;
+	//tabela onde sera armazenado os resultados
+	//O = Ocioso
+	//P = Pronto
+	//E = Execucao
+	//I = Indisponivel
+	//F = Finalizado
+	//T = Troca de contexto
+
+	matriz = (char**)malloc(tempoTotal * sizeof(char*));
+	for (i = 0; i<tempoTotal; i++)
+	{
+		matriz[i] = (char*)malloc(nProcessos * sizeof(char));
+	}
+	//char matriz[nExec][nProcessos]
+
+	int j;
+	for (i = 0; i<tempoTotal; i++)
+	{	// i marca tempo de cpu
+		for (j = 0; j<nProcessos; j++)
+		{	// j marca processo
+			if (vInitProcessos[j] > i)
+			{
+				matriz[i][j] = 'I';
+			}//Caso o processo ainda nao tenha sido criado
+			else if (i >= vFimProcessos[j])
+			{
+				matriz[i][j] = 'F';
+			}//Caso ja tenha terminado
+			else if(vProcessos[i]==-1)	 
+			{							  
+				matriz[i][j] = 'O';
+			}//Caso a cpu esteja ociosa
+			else if (vProcessos[i] == -2)
+			{
+				matriz[i][j] = 'T';
+			}//Caso a cpu esteja trocando o contexto
+			else if (vProcessos[i] == j)
+			{
+				matriz[i][j] = 'E';
+			}//Caso seja o processo executando
+			else
+			{
+				matriz[i][j] = 'P';
+			}//Caso esteja na fila de pronto
+		}
+	}
+	//cria a tabela que salva os estados dos processos em cada tempo
+
+	#pragma endregion
+
+
+	//----------------------------------------------------
+	// ARMAZENAMENTO EM DISCO DOS RESULTADOS
+	//----------------------------------------------------
+	#pragma region Armazenamento em Disco dos Resultados
+
+	FILE *fp;
+	fp = fopen("resultado.txt", "w");
+	//arquivo onde será armazenado a tabela de resultado
+
+
+	fprintf(fp, "Processo\t");
+	for (i = 0; i<tempoTotal; i++)
+		fprintf(fp, "%d\t", i);
+	fprintf(fp, "Turnaround");
+
+	for (j = 0; j<nProcessos; j++)
+	{	//j marca processos
+		fprintf(fp, "\n%d\t\t", j);
+		for (i = 0; i<tempoTotal; i++)
+		{	//i marca tempo
+			fprintf(fp, "%c\t", matriz[i][j]);
+		}
+		fprintf(fp, "%d", vFimProcessos[j] - vInitProcessos[j]);
+	}
+	//salva resultado no arquivo
+
+	/*
+	Exemplo de resultado:
+	Processo	0	1	2	3	4	.	.	.	Turnaround
+	0			I	I	P	E	F				4
+	1			T	E	F	F	F				2
+	2			T	P	E	F	F				3
+	.			.					.			.
+	.			.						.		.
+	.			.							.	.
+	*/
+
+	fclose(fp);
+
+	system("resultado.txt");
+
+	return EXIT_SUCCESS;
+
+	#pragma endregion
+
+}
+
+
+#pragma region Implementacoes
+
+void Enfileira(Lista *l, Processo p, int algoritmo)
+// Funcao que escolhe o procedimento de insercao adequado ao algoritmo
+{
+	Processo *aux;
+	aux = (Processo*)malloc(sizeof(Processo));
+	*aux = p;
+	//a lista precisa que uma posicao de memoria nova seja alocada a cada insercao
+
+	switch (algoritmo)
+	{
+		case 1: //FIFO
+		case 3: //Circular
+			InsereFimLista(l, aux);
+			break;
+		case 2: //SJF
+			InsereOrdenadoLista(l, aux, ins_tempoCPU);
+			break;
+		case 4: //Prioridade
+			InsereOrdenadoLista(l, aux, ins_prioridade);
+			break;
+		default: //Erro
+			exit(EXIT_FAILURE);
+	}
+}
+
+#pragma region Funcoes Auxiliares de Ordenacao
+
+int ins_tempoCriacao(void *a, void *b)
+//Retorna true caso o processo a seja criado antes de b, false caso contrário
+{
+	Processo *x, *y;
+	x = a;
+	y = b;
+	return x->tempoChegada <= y->tempoChegada;
+} 
+
+int ins_tempoCPU(void *a, void *b)
+//Retorna true caso o processo a tenha um tempo de cpu menor que b, false caso contrário
+{
+	Processo *x, *y;
+	x = a;
+	y = b;
+	return x->tempoCpu < y->tempoCpu;
+}
+
+int ins_prioridade(void *a, void *b)
+//Retorna true caso o processo a tenha uma prioridade maior que b, false caso contrário
+{
+	Processo *x, *y;
+	x = a;
+	y = b;
+	return x->prioridade > y->prioridade;
+}
+
+#pragma endregion Funcoes usadas para inserir ordenado na lista, cada uma segundo um parametro
+
+#pragma endregion Implementacao das funcoes usadas na simulacao
